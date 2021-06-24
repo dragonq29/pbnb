362aa10251c65c019b7680ebff96309d98d86e85
DragonQ <dragonq29@gmail.com>
2021-06-24 23:11:26 +0900

    날짜, 빠밥/늦밥 표시 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
+import gfood from './gfood';
+
+const today = new Date();
+const day = today.getDay();
+const dayNames = ['(일요일)', '(월요일)', '(화요일)', '(수요일)', '(목요일)', '(금요일)', '(토요일)'];
+const month = today.getMonth() + 1;
+const date = today.getDate();
+const pbnb = (month + day) % 2 === 0; // true : 빠밥
+
 
 function App() {
   return (
     <div>
-      첫 페이지 테스트
+      <div id="header">
+        <h1>{month}/{date} {dayNames[day]}</h1>
+        <h3>오늘은 {pbnb ? "빠밥" : "늦밥"}</h3>
+      </div>
     </div>
   );
 }
---
+++ src/gfood.js
@@ -0,0 +1,7 @@
+export default function(startDate, endDate, bizPlaceCode) {
+    return {
+        st_dt: startDate,
+        end_dt: endDate,
+        bizplc_cd: bizPlaceCode
+    }
+}
