4a66dc5d522cc753ebed2443ec0b160a409baf05
DragonQ <dragonq29@gmail.com>
2021-09-22 19:22:16 +0900

    코드 리팩토링

--- src/Meal.js
+++ src/Meal.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function Meal({ data }) {
+function Meal({ data, children }) {
   console.log(data);
   return (
     <div>
+      <div>{children}</div>
       <ul>
         {data.map((menu, index) => (
           <li key={index}>{menu.name}</li>
