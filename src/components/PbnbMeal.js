87d6d978a92b03635a51808e3803f6ce0c113274
DragonQ <dragonq29@gmail.com>
2021-09-27 23:01:28 +0900

    글자 크기 수정

--- src/components/PbnbMeal.js
+++ src/components/PbnbMeal.js
@@ -5,8 +5,7 @@ import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 const PbnbMealBlock = styled.div`
   display: flex;
   align-items: center;
-  padding-top: 12px;
-  padding-bottom: 12px;
+  font-size: 20px;
 `;
 
 const upArrow = styled.div`
@@ -16,23 +15,25 @@ const upArrow = styled.div`
 function PbnbMeal({ data, children }) {
   return (
     <div>
-      <div>{children}</div>
-      <ul>
-        {data != null
-          ? data.map((menu, index) => (
-              <li key={index}>
-                {menu.mealName} ({menu.mainMenuName})
-                {
-                  <ul>
-                    {menu.list.map((subMenu, subIndex) => (
-                      <li key={subIndex}>{subMenu}</li>
-                    ))}
-                  </ul>
-                }
-              </li>
-            ))
-          : "로딩중"}
-      </ul>
+      <div style={{ fontSize: "30px" }}>{children}</div>
+      <PbnbMealBlock>
+        <ul>
+          {data != null
+            ? data.map((menu, index) => (
+                <li key={index}>
+                  {menu.mealName} ({menu.mainMenuName})
+                  {
+                    <ul>
+                      {menu.list.map((subMenu, subIndex) => (
+                        <li key={subIndex}>{subMenu}</li>
+                      ))}
+                    </ul>
+                  }
+                </li>
+              ))
+            : "로딩중"}
+        </ul>
+      </PbnbMealBlock>
     </div>
   );
 }
